3ed74662944942fc84e710f2826b05092e089acf
Anasmalik57 <itsanas474@gmail.com>
2025-07-20 13:25:22 +0530

    Structure optimized with minor changes

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,30 @@ import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
 
 // Creates a SQL connection using our DB URL
-export const sql = neon(process.env.DATABASE_URL ||  'postgresql://neondb_owner:npg_cpEohWG35IvZ@ep-misty-silence-aefvezna-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
-console.log('====================================');
+export const sql = neon(
+  process.env.DATABASE_URL ||
+    "postgresql://neondb_owner:npg_cpEohWG35IvZ@ep-misty-silence-aefvezna-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+);
+console.log("====================================");
 console.log(process.env.DATABASE_URL);
-console.log('====================================');
+console.log("====================================");
+
+// ====================================
+export const initDB = async () => {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL ,
+    title VARCHAR(255) NOT NULL ,
+    amount DECIMAL(10,2) NOT NULL ,
+    category VARCHAR(255) NOT NULL ,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE    
+    )`;
+    console.log("====================================");
+    console.log("Database initialized Successfully 😄");
+    console.log("====================================");
+  } catch (error) {
+    console.log("Error Initializing DB ===> ", error);
+    process.exit(1); // status code 1 means faliure and 0 means success
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/connectDB.js";
+import { initDB } from "./config/connectDB.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionRoute.js";
 
@@ -12,27 +12,6 @@ const app = express();
 app.use(rateLimiter);
 app.use(express.json());
 
-export const initDB = async () => {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-    id SERIAL PRIMARY KEY,
-    user_id VARCHAR(255) NOT NULL ,
-    title VARCHAR(255) NOT NULL ,
-    amount DECIMAL(10,2) NOT NULL ,
-    category VARCHAR(255) NOT NULL ,
-    created_at DATE NOT NULL DEFAULT CURRENT_DATE    
-    )`;
-    console.log("====================================");
-    console.log("Database initialized Successfully 😄");
-    console.log("====================================");
-  } catch (error) {
-    console.log("Error Initializing DB ===> ", error);
-    process.exit(1); // status code 1 means faliure and 0 means success
-  }
-};
-app.get("/", (req, res) => {
-  res.send("it's working buddy");
-});
 // ---------------------------------------------------
 
 app.use("/api/transactions", transactionsRoute);
