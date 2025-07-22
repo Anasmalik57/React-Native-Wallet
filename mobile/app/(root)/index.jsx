2aba0a8c8210e17070bde02945ec4281dff3ce2a
Anasmalik57 <itsanas474@gmail.com>
2025-07-22 14:57:03 +0530

    feched data successfully from the backend

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,10 +1,27 @@
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
-import { SignOutButton } from '@/components/SignOutButton'
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SignOutButton } from "@/components/SignOutButton";
+import { useTransactions } from "../../hooks/useTransaction";
+import { useEffect } from "react";
 
 export default function Page() {
-  const { user } = useUser()
+  const { user } = useUser();
+
+  const { transactions, summary, isLoading, loadData, deleteTransactions } =
+    useTransactions(user.id);
+
+  useEffect(() => {
+    loadData();
+  }, [user?.id]);
+
+  console.log("====================================");
+  console.log("userId 😃:===>", user.id);
+  console.log("====================================");
+  // setTimeout(() => {
+    console.log("Transactions: ==>", JSON.stringify(transactions, null, 2));
+    console.log("Summary: ==>", JSON.stringify(summary, null, 2));
+  // }, 2000)
 
   return (
     <View>
@@ -21,5 +38,5 @@ export default function Page() {
         </Link>
       </SignedOut>
     </View>
-  )
+  );
 }
---
+++ mobile/hooks/useTransaction.js
@@ -0,0 +1,70 @@
+// react custom hook file
+
+import { useCallback, useState } from "react";
+import { Alert } from "react-native";
+
+const API_URL = "http://192.168.31.171:5001/api";
+
+export const useTransactions = (userId) => {
+  const [transactions, setTransactions] = useState([]);
+  const [summary, setSummary] = useState({
+    balance: 0,
+    income: 0,
+    expenses: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  // ====================================
+  // usecallback is used for performance reasons, it will memoize the function
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${userId}`);
+      const data = await response.json();
+      setTransactions(data);
+    } catch (error) {
+      console.error("🔴 Error fetching transactions: ==> ", error);
+    }
+  }, [userId]);
+
+  // ==========================================
+  const fetchSummary = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/summary/${userId}`);
+      const data = await response.json();
+      setSummary(data);
+    } catch (error) {
+      console.error("🔴 Error fetching Summary: ==> ", error);
+    }
+  }, [userId]);
+  // ==========================================
+  const loadData = useCallback(async () => {
+    if (!userId) return;
+    setIsLoading(true);
+    try {
+      // these functions can be run in parallel || we can await them one by one also
+      await Promise.all([fetchTransactions(), fetchSummary()]);
+    } catch (error) {
+      console.error("🔴 Error loading Data: ==> ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [fetchTransactions, fetchSummary, userId]);
+
+  // ==============================================
+  const deleteTransaction = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) throw new Error("Failed to delete transaction");
+
+      // refresh data after deletion
+      loadData();
+      Alert.alert("Success", "Transaction deleted successfully");
+    } catch (error) {
+      console.error("🔴 Error Deleting Transaction: ==> ", error);
+      Alert.alert("Error", error.message);
+    }
+  };
+  return { transactions, summary, isLoading, loadData, deleteTransaction };
+};
